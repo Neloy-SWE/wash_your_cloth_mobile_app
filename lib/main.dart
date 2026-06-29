a36f25602f3472f734efc807ce3b883fd04c9198
Neloy-SWE <taufiqneloy.swe@gmail.com>
2026-06-29 21:11:45 +0600

    dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -5,35 +5,96 @@ Email: taufiqneloy.swe@gmail.com
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wash_your_cloth_mobile_app/data/client/client.dart';
+import 'package:wash_your_cloth_mobile_app/data/network/api_call/api_refresh_token.dart';
+import 'package:wash_your_cloth_mobile_app/data/repository/repository_authentication.dart';
 import 'package:wash_your_cloth_mobile_app/presentation/bloc_global/global_bloc.dart';
 import 'package:wash_your_cloth_mobile_app/router/app_router.dart';
 import 'package:wash_your_cloth_mobile_app/utilities/app_text.dart';
 import 'package:wash_your_cloth_mobile_app/utilities/app_theme.dart';
 
+import 'data/local/local_storage_service.dart';
+
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  final localStorageService = LocalStorageService();
+
+  final client = Client();
+  final apiRefreshToken = ApiRefreshToken(client: client);
+
+  final repositoryAuthentication = RepositoryAuthentication(
+    localStorageService: localStorageService,
+    apiRefreshToken: apiRefreshToken,
+  );
+
+  final globalBloc = GlobalBloc(
+    repositoryAuthentication: repositoryAuthentication,
+  )..add(GlobalEventGetLoginStatus());
+
+  final appRouter = AppRouter(globalBloc: globalBloc);
+
+  runApp(
+    MyApp(
+      localStorageService: localStorageService,
+      client: client,
+      apiRefreshToken: apiRefreshToken,
+      repositoryAuthentication: repositoryAuthentication,
+      globalBloc: globalBloc,
+      appRouter: appRouter,
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final LocalStorageService localStorageService;
+  final Client client;
+  final IApiRefreshToken apiRefreshToken;
+  final IRepositoryAuthentication repositoryAuthentication;
+  final GlobalBloc globalBloc;
+  final AppRouter appRouter;
+
+  const MyApp({
+    super.key,
+    required this.localStorageService,
+    required this.client,
+    required this.apiRefreshToken,
+    required this.repositoryAuthentication,
+    required this.globalBloc,
+    required this.appRouter,
+  });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        routerConfig: appRouter,
-        theme: AppTheme.get,
-        title: AppText.appTitle,
-        builder: (context, child) {
-          return MediaQuery(
-            data: MediaQuery.of(
-              context,
-            ).copyWith(textScaler: TextScaler.noScaling),
-            child: BlocProvider(create: (_) => GlobalBloc(), child: child!),
-          );
-        },
+      child: MultiRepositoryProvider(
+        providers: [
+          RepositoryProvider<LocalStorageService>.value(
+            value: localStorageService,
+          ),
+          RepositoryProvider<Client>.value(value: client),
+          RepositoryProvider<IApiRefreshToken>.value(value: apiRefreshToken),
+          RepositoryProvider<IRepositoryAuthentication>.value(
+            value: repositoryAuthentication,
+          ),
+        ],
+        child: BlocProvider<GlobalBloc>.value(
+          value: globalBloc,
+          child: MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            routerConfig: appRouter.door,
+            theme: AppTheme.get,
+            title: AppText.appTitle,
+            builder: (context, child) {
+              return MediaQuery(
+                data: MediaQuery.of(
+                  context,
+                ).copyWith(textScaler: TextScaler.noScaling),
+                child: child!,
+              );
+            },
+          ),
+        ),
       ),
     );
   }
