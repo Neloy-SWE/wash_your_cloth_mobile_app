8813458457960ae29981360cd67328bc3dc068dc
Neloy-SWE <taufiqneloy.swe@gmail.com>
2026-06-29 21:10:11 +0600

    local data storage service added

---
+++ lib/data/local/local_storage_service.dart
@@ -0,0 +1,87 @@
+/* 
+Created by Neloy on 29 June, 2026.
+Email: taufiqneloy.swe@gmail.com
+*/
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:wash_your_cloth_mobile_app/data/client/client_constant.dart';
+
+class LocalStorageService {
+  final FlutterSecureStorage _secureStorage;
+
+  LocalStorageService({FlutterSecureStorage? secureStorage})
+    : _secureStorage = secureStorage ?? FlutterSecureStorage();
+
+  Future<void> saveAuthData({
+    required String token,
+    required String refreshToken,
+    required String expirationToken,
+    required String expirationRefreshToken,
+  }) async {
+    await _secureStorage.write(key: ClientConstant.token, value: token);
+    await _secureStorage.write(
+      key: ClientConstant.refreshToken,
+      value: refreshToken,
+    );
+    await _secureStorage.write(
+      key: ClientConstant.expirationToken,
+      value: expirationToken,
+    );
+    await _secureStorage.write(
+      key: ClientConstant.expirationRefreshToken,
+      value: expirationRefreshToken,
+    );
+  }
+
+  Future<String?> getToken() => _secureStorage.read(key: ClientConstant.token);
+
+  Future<String?> getRefreshToken() =>
+      _secureStorage.read(key: ClientConstant.refreshToken);
+
+  Future<String?> getTokenExpiration() =>
+      _secureStorage.read(key: ClientConstant.expirationToken);
+
+  Future<String?> getRefreshTokenExpiration() =>
+      _secureStorage.read(key: ClientConstant.expirationRefreshToken);
+
+  Future<void> clearAuthData() async {
+    await _secureStorage.deleteAll();
+  }
+
+  Future<String> loginStatus() async {
+    try {
+      final token = await getToken();
+      final refreshToken = await getRefreshToken();
+      final tokenExpiration = await getTokenExpiration();
+      final refreshTokenExpiration = await getRefreshTokenExpiration();
+
+      // print("token::: $token");
+      // print("refreshToken::: $refreshToken");
+      // print("tokenExpiration::: $tokenExpiration");
+      // print("refreshTokenExpiration::: $refreshTokenExpiration");
+
+      if (token == null || refreshToken == null) {
+        return ClientConstant.selectRole;
+      }
+
+      final nowDT = DateTime.now();
+      final tokenExpirationDT = DateTime.parse(tokenExpiration!);
+
+      if (tokenExpirationDT.isAfter(nowDT)) {
+        return ClientConstant.dashboard;
+      }
+
+      final refreshTokenExpirationDT = DateTime.parse(refreshTokenExpiration!);
+
+      if (refreshTokenExpirationDT.isAfter(nowDT)) {
+        return ClientConstant.tokenRefresh;
+      } else {
+        return ClientConstant.selectRole;
+      }
+    } catch (e) {
+      await clearAuthData();
+      // return ClientConstant.retry;
+      return ClientConstant.selectRole;
+    }
+  }
+}
