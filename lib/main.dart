a4481a2b778cf2d39ecde5ef62ef7bc0f600d797
Neloy-SWE <taufiqneloy.swe@gmail.com>
2026-07-02 23:01:13 +0600

    auth dependency added

--- lib/main.dart
+++ lib/main.dart
@@ -6,62 +6,35 @@ Email: taufiqneloy.swe@gmail.com
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wash_your_cloth_mobile_app/data/client/client.dart';
+import 'package:wash_your_cloth_mobile_app/data/network/api_call/api_login.dart';
 import 'package:wash_your_cloth_mobile_app/data/network/api_call/api_refresh_token.dart';
 import 'package:wash_your_cloth_mobile_app/data/repository/repository_authentication.dart';
 import 'package:wash_your_cloth_mobile_app/presentation/bloc_global/global_bloc.dart';
+import 'package:wash_your_cloth_mobile_app/presentation/screen/authentication/bloc/authentication_bloc.dart';
 import 'package:wash_your_cloth_mobile_app/router/app_router.dart';
 import 'package:wash_your_cloth_mobile_app/utilities/app_text.dart';
 import 'package:wash_your_cloth_mobile_app/utilities/app_theme.dart';
 
 import 'data/local/local_storage_service.dart';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wash_your_cloth_mobile_app/data/client/client.dart';
+import 'package:wash_your_cloth_mobile_app/data/network/api_call/api_refresh_token.dart';
+import 'package:wash_your_cloth_mobile_app/data/repository/repository_authentication.dart';
+import 'package:wash_your_cloth_mobile_app/presentation/bloc_global/global_bloc.dart';
+import 'package:wash_your_cloth_mobile_app/router/app_router.dart';
+import 'package:wash_your_cloth_mobile_app/utilities/app_text.dart';
+import 'package:wash_your_cloth_mobile_app/utilities/app_theme.dart';
+import 'data/local/local_storage_service.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  final localStorageService = LocalStorageService();
-
-  final client = Client();
-  final apiRefreshToken = ApiRefreshToken(client: client);
-
-  final repositoryAuthentication = RepositoryAuthentication(
-    localStorageService: localStorageService,
-    apiRefreshToken: apiRefreshToken,
-  );
-
-  final globalBloc = GlobalBloc(
-    repositoryAuthentication: repositoryAuthentication,
-  )..add(GlobalEventGetLoginStatus());
-
-  final appRouter = AppRouter();
-
-  runApp(
-    MyApp(
-      localStorageService: localStorageService,
-      client: client,
-      apiRefreshToken: apiRefreshToken,
-      repositoryAuthentication: repositoryAuthentication,
-      globalBloc: globalBloc,
-      appRouter: appRouter,
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final LocalStorageService localStorageService;
-  final Client client;
-  final IApiRefreshToken apiRefreshToken;
-  final IRepositoryAuthentication repositoryAuthentication;
-  final GlobalBloc globalBloc;
-  final AppRouter appRouter;
-
-  const MyApp({
-    super.key,
-    required this.localStorageService,
-    required this.client,
-    required this.apiRefreshToken,
-    required this.repositoryAuthentication,
-    required this.globalBloc,
-    required this.appRouter,
-  });
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -69,20 +42,53 @@ class MyApp extends StatelessWidget {
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: MultiRepositoryProvider(
         providers: [
-          RepositoryProvider<LocalStorageService>.value(
-            value: localStorageService,
+          RepositoryProvider<LocalStorageService>(
+            create: (context) => LocalStorageService(),
           ),
-          RepositoryProvider<Client>.value(value: client),
-          RepositoryProvider<IApiRefreshToken>.value(value: apiRefreshToken),
-          RepositoryProvider<IRepositoryAuthentication>.value(
-            value: repositoryAuthentication,
+
+          RepositoryProvider<Client>(create: (context) => Client()),
+
+          RepositoryProvider<IApiRefreshToken>(
+            create: (context) =>
+                ApiRefreshToken(client: RepositoryProvider.of<Client>(context)),
           ),
+
+          RepositoryProvider<IApiLogin>(
+            create: (context) =>
+                ApiLogin(client: RepositoryProvider.of<Client>(context)),
+          ),
+
+          RepositoryProvider<IRepositoryAuthentication>(
+            create: (context) => RepositoryAuthentication(
+              localStorageService: RepositoryProvider.of<LocalStorageService>(
+                context,
+              ),
+              apiRefreshToken: RepositoryProvider.of<IApiRefreshToken>(context),
+              apiLogin: RepositoryProvider.of<IApiLogin>(context),
+            ),
+          ),
+
+          // RepositoryProvider<AppRouter>(create: (context) => AppRouter()),
         ],
-        child: BlocProvider<GlobalBloc>.value(
-          value: globalBloc,
+        child: MultiBlocProvider(
+          providers: [
+            BlocProvider<GlobalBloc>(
+              create: (context) => GlobalBloc(
+                repositoryAuthentication:
+                    RepositoryProvider.of<IRepositoryAuthentication>(context),
+              )..add(GlobalEventGetLoginStatus()),
+            ),
+
+            BlocProvider<AuthenticationBloc>(
+              create: (context) => AuthenticationBloc(
+                repositoryAuthentication:
+                    RepositoryProvider.of<IRepositoryAuthentication>(context),
+              ),
+            ),
+          ],
           child: MaterialApp.router(
             debugShowCheckedModeBanner: false,
-            routerConfig: appRouter.door,
+            routerConfig: AppRouter.door,
             theme: AppTheme.get,
             title: AppText.appTitle,
             builder: (context, child) {
